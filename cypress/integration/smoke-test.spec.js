a44aee7adf97c286717a8c4781d5b3a5537e440c
Vishnu Kugarthi <vishnutejaap@gmail.com>
2021-11-11 17:37:34 +0530

    changes

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -6,13 +6,15 @@ describe('The application loads', () => {
 
   it('has the basic Todo list container', () => {
     cy.get('.todo-list').should('exist');
+    // cy.screenshot();
   });
 
   it('has the auto focused and enter a value', () => {
-    const item1 = 'Buy milk';
+    const item1 = 'Buy vegetables';
 
     cy.focused().should('have.class', 'new-todo');
     cy.get('.new-todo').type(item1).should('have.value', item1);
+    // cy.screenshot();
     // cy.get('.new-todo')
   });
 });
